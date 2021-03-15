a2424c3f62e4b87a44f9c182415b7a3062aa76f4
MonteyMontey <kleindominik1@gmx.de>
2021-03-15 16:13:23 +0100

    add common algorithm interface

---
+++ rl_algorithms/rl_algorithm_interface.py
@@ -0,0 +1,10 @@
+class RLAlgorithmInterface:
+
+    def choose_action(self, observation: list) -> object:
+        pass
+
+    def remember(self, state: list, action: object, reward: float, state_: list, done: bool) -> None:
+        pass
+
+    def learn(self) -> None:
+        pass

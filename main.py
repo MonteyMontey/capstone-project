7d275fb9486fc000801ba5b570e631d7f4e8e4ae
MonteyMontey <kleindominik1@gmx.de>
2021-03-29 18:08:25 +0200

    overwrite sys exception handler to see proper traceback

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from gui.app import MainWindow
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
+    sys._excepthook = sys.excepthook
+
+
+    def my_exception_hook(exctype, value, traceback):
+        # Print the error and traceback
+        print(exctype, value, traceback)
+        # Call the normal Exception hook after
+        sys._excepthook(exctype, value, traceback)
+        sys.exit(1)
+
+
+    sys.excepthook = my_exception_hook
+
     window = MainWindow()
     window.show()
 
